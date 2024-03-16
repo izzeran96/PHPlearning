c12ed2eb28b84b6fc53eb38d08af3ebac1106908
IZZERAN SHAH <79451664+izzeran96@users.noreply.github.com>
2024-03-16 19:14:14 +0800

    Update index.php

--- phpl8/index.php
+++ phpl8/index.php
@@ -1,18 +1,12 @@
 <?php
 
 require 'functions.php';
-function dd($value){
- echo "";
- var_dump($value);
- echo "";
-die();
-}
-
-dd($value);
 
 function urlIs($value){
- return $_SERVER['REQUEST_URL'];
+ return $_SERVER['REQUEST_URL'] === $value;
 }
+
+if(urlIs('/')){{
 
 
  ?>
