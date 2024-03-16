5f7aa87284a987cf9c13cb4b689fdcdfcbd22c73
IZZERAN SHAH <79451664+izzeran96@users.noreply.github.com>
2024-03-16 15:31:38 +0800

    Update

---
+++ filterfunction.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+ $books = [
+   [
+  'title' => 'The story malaysia',
+   'author' => 'ahmad samsudin',
+   'purchaseUrl' = 'http://example.com'
+  ],
+    [
+'title' => 'Sundaland and nusantara',
+   'author' => 'ari pribowo',
+   'purchaseUrl' = 'http://example.com'
+]
+];
+?>
+
+<ul>
+
+<?php foreach($books as $book) : ?>
+<li>
+<?= $book['title'] ?>
+</li>
+<?= endforeach; ?>
+</ul>
+
+
+</body>
+</html>
