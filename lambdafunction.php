ba51e2b4a67ddad0ff5972295c3482e2bd29db11
IZZERAN SHAH <79451664+izzeran96@users.noreply.github.com>
2024-03-16 16:23:54 +0800

    Update

---
+++ lambdafunction.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+ $books = [
+   [
+  'title' => 'The story malaysia',
+   'author' => 'ahmad samsudin',
+   'purchaseUrl' => 'http://example.com'
+  ],
+    [
+'title' => 'Sundaland and nusantara',
+   'author' => 'ari pribowo',
+   'purchaseUrl' => 'http://example.com'
+]
+];
+
+ function filterByAuthor($books,$author){
+ $filteredBooks = [];
+  foreach ($books as $book){
+if ($book['author'] ===  $author){
+  $filteredBooks[] = $book;
+}
+}
+  return $filteredBooks;
+}
+
+  $filterByAuthor = filterByAuthor($books,$author);
+?>
+
+<ul>
+
+<?php foreach ($filterByAuthor($books,'ari pribowo') as $book) : ?>
+<li>
+<?= $book['title'] ?>
+</li>
+<?php endforeach; ?>
+</ul>
+
+
+</body>
+</html>
