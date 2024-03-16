04f55b35a3a0ce5dbfb83887fff323c4f25d99ed
IZZERAN SHAH <79451664+izzeran96@users.noreply.github.com>
2024-03-16 17:23:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
+ $books = [
+   [
+  'title' => 'The story malaysia',
+   'author' => 'ahmad samsudin',
+   'purchaseUrl' = 'http://example.com'
+  ],
+    [
+'title' => 'Sundaland and nusantara',
+   'author' => 'ari pribowo',
+   'purchaseUrl' = 'http://example.com'
+]
+];
+
+$filteredBook = array_filter($books,function($book){
+ return $book[] === 
+});
+
 require 'index.viex.php';
 ?>
